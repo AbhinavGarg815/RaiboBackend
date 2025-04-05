65cc50ce737c5fa19b2df19d5bab6e1928440289
Abhinav Garg <abhinavgrg815@gmail.com>
2025-04-05 15:31:04 +0530

    implement logout and refresh token functionality; update auth routes to include new endpoints

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from '../models/user.model.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const registerUser = asyncHandler(async (req, res) => {
     if (!req.body) {
@@ -52,10 +53,15 @@ const loginUser = asyncHandler(async (req, res) => {
     }
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 1000 * 60 * 60 * 24 * 10
+    })
     res.status(200).json({
         message: "Login successful",
         accessToken,
-        refreshToken,
         user: {
             _id: user._id,
             fullname: user.fullname,
@@ -65,4 +71,39 @@ const loginUser = asyncHandler(async (req, res) => {
     });
 })
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+    res.clearCookie('refreshToken', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax'
+    })
+    res.status(200).json({
+        message: "Logged out successfully"
+    })
+})
+
+const refreshToken = asyncHandler (async (req, res) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+        return res.status(403).json({ message: "Refresh token not found" });
+    }
+    
+    let decoded;
+    try {
+        decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    } catch (error) {
+        return res.status(403).json({ message: "Invalid refresh token" });
+    }
+
+    const user = await User.findById(decoded._id);
+    if (!user) {
+        return res.status(403).json({ message: "User not found" });
+    }
+    const newAccessToken = user.generateAccessToken();
+
+    return res.status(200).json({
+        accessToken: newAccessToken
+    });
+});
+
+export { registerUser, loginUser, logoutUser, refreshToken };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
-import { loginUser, registerUser } from '../controllers/auth.controller.js';
+import { loginUser, registerUser, logoutUser, refreshToken,  } from '../controllers/auth.controller.js';
 import { Router } from "express";
 
 const router = Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/logout', logoutUser);
+router.post('/refresh', refreshToken);
 
 export default router
