778a296fc28d2ee2709006961987236109c8e188
Omkesh <shmokedev@gmail.com>
2025-04-11 03:08:36 +0530

    add product route validators

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct, getProductsByCategory, getProductsByCompany } from '../controllers/product.controller.js';
 import { jwtAuthenticator } from '../middlewares/passport.middleware.js';
+import { productCreateValidator, productDeleteValidator, productGetByCategoryValidator, productGetByCompanyValidator, productGetByIdValidator, productGetValidator, productUpdateValidator } from '../middlewares/product.validator.middleware.js';
 
 const router = Router();
 
-router.post('/', jwtAuthenticator, createProduct);
-router.get('/', getAllProducts);
-router.get('/:id', getProductById);
-router.put('/:id', jwtAuthenticator, updateProduct);
-router.delete('/:id', jwtAuthenticator, deleteProduct);
-router.get('/category/:category_id', getProductsByCategory);
-router.get('/company/:company_id', getProductsByCompany);
+router.post('/', jwtAuthenticator, productCreateValidator, createProduct);
+router.get('/', productGetValidator,getAllProducts);
+router.get('/:id', productGetByIdValidator,getProductById);
+router.put('/:id', jwtAuthenticator,productUpdateValidator, updateProduct);
+router.delete('/:id', jwtAuthenticator, productDeleteValidator,deleteProduct);
+router.get('/category/:category_id', productGetByCategoryValidator,getProductsByCategory);
+router.get('/company/:company_id', productGetByCompanyValidator,getProductsByCompany);
 
 export default router;
