42479be93a2a1619e5a937f3fc51df00b43fc2e5
Omkesh <shmokedev@gmail.com>
2025-04-14 00:35:46 +0530

    minor changes in image route

--- src/routes/images.routes.js
+++ src/routes/images.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { upload , multerErrorHandler } from '../middlewares/validators/multer.middleware.js';
+import { upload , multerErrorHandler } from '../middlewares/multer.middleware.js';
 import { uploadImage, deleteImage, getImageById} from '../controllers/image.controller.js';
 import { getImageByIdValidator, deleteImageByIdValidator} from '../middlewares/validators/image.validator.js';
+import { jwtAuthenticator } from '../middlewares/passport.middleware.js';
 
 const router = express.Router();
 
-router.post('/upload', upload.single('image') , multerErrorHandler,uploadImage);
+router.post('/upload', jwtAuthenticator,upload.single('image') , multerErrorHandler,uploadImage);
 router.get('/:id', getImageByIdValidator, getImageById);
 router.get('/delete/:id',deleteImageByIdValidator , deleteImage);
 
